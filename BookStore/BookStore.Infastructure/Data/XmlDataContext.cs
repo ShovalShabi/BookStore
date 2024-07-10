9d06e313d744e065f9bdd28b55ae4b679adf4778
ShovalShabi <shovalshabi@gmail.com>
2024-07-10 20:11:40 +0300

    Added Logging to the XML context class

--- BookStore/BookStore.Infastructure/Data/XmlDataContext.cs
+++ BookStore/BookStore.Infastructure/Data/XmlDataContext.cs
@@ -1,21 +1,44 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Xml.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 public class XmlDataContext
 {
     private readonly string _filePath;
+    private readonly ILogger<XmlDataContext> _logger;
 
-    public XmlDataContext(IConfiguration configuration)
+    public XmlDataContext(IConfiguration configuration, ILogger<XmlDataContext> logger)
     {
         _filePath = configuration.GetSection("DataConfiguration:FilePath").Value;
+        _logger = logger;
     }
 
     public XDocument LoadXml()
     {
-        return XDocument.Load(_filePath);
+        try
+        {
+            _logger.LogInformation($"Loading XML from file: {_filePath}");
+            return XDocument.Load(_filePath);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error loading XML from file: {_filePath}");
+            throw;
+        }
     }
 
     public void SaveXml(XDocument document)
     {
-        document.Save(_filePath);
+        try
+        {
+            _logger.LogInformation($"Saving XML to file: {_filePath}");
+            document.Save(_filePath);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error saving XML to file: {_filePath}");
+            throw;
+        }
     }
 }
