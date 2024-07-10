dd1861da5d9c29d4ddaccbf7f5a37f0435c7fe78
ShovalShabi <shovalshabi@gmail.com>
2024-07-10 12:27:14 +0300

    Added logging configurations to the application

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,13 +1,10 @@
 using Bookstore.Application.Interfaces;
 using Bookstore.Domain.Repositories;
-using Microsoft.Win32;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,6 +13,10 @@ builder.Services.AddScoped<XmlDataContext>(); // Register XmlDataContext
 builder.Services.AddScoped<ReportGenerator>();
 builder.Services.AddScoped<IBookRepository, BookRepository>(); // Register BookRepository implementation
 builder.Services.AddScoped<IBookService, BookService>(); // Register BookService implementation
+
+// Configure logging
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole(); // Log to the console
 
 var app = builder.Build();
 
@@ -27,7 +28,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
